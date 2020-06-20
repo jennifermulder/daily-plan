b1607b008fabf168c70035f623b9793b4fecfb12
J Mulder <jlmulder2014@gmail.com>
2020-06-20 16:33:44 -0700

    detailed comments and formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,9 @@
-// var currentHour = 0;
-
-// let hourArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-
-// var row1 = $("#9").children();
-// var hour = parseInt(`${hourArr[currentHour]}`);
-// console.log(hour);
-
-// var row2 = $("#10").children();
-// var hour = parseInt(`${hourArr[1]}`);
-// console.log(hour);
-
-
-// var row3 = $("#11").children();
-// var hour = parseInt(row3[0].textContent.trim());
-// console.log(hour);
 
 //current date 
 var date = $("#currentDay").text(`${moment().format("dddd, MMMM Do")}`);
 
 
-//Use to audit current time against time in far left block & change color
+//Audit current time against time in far left block & change color of text field
 var auditTask = function () {
   for (let i = 0; i < 24; i++) {
     const $div = $("div#" + i);
@@ -54,49 +38,13 @@ setInterval(function () {
   auditTask();
 }, 1800000);
 
-console.log(setInterval);
 
-
-// value of text is changed
-$("textarea").on("change", "input[type='text']", function () {
-  console.log(62)
-
-
-
-
-  // get current text
-  var description = $(this)
-    .val()
-    .trim();
-  console.log(textarea);
-
-  // get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".description")
-    .index();
-
-  // update task in array and re-save to localstorage
-  tasks[index].description = description;
-  saveTasks();
-
-  // Rerun audit to check new due date
-  auditTask();
-});
-
-
-
-
-// save tasks with jQuery 
+// When save button is clicked: save task time and description to array
 $(".saveBtn").click(function (event) {
-  // console.log(event.target);
 
   var buttonClicked = $(event.target).attr('data-name');
   var taskTime = buttonClicked;
   var taskDescription = $("#text-area-" + buttonClicked).val().trim();
-  console.log(taskTime);
-  console.log(taskDescription);
-  //  console.log("line98 =>", taskDescription.val().trim());
-  //  console.log("line99 =>", taskDescription[0].value.trim());
 
   var task = {
     id: taskTime,
@@ -108,8 +56,9 @@ $(".saveBtn").click(function (event) {
 });
 
 //helpers here
-
+//save task array to local storage
 function saveTasks(task) {
+  //find any items currently saved in local storage
   var tasks = JSON.parse(localStorage.getItem("tasks"));
   if (!tasks) {
     tasks = [];
@@ -118,13 +67,11 @@ function saveTasks(task) {
   tasks.push(task);
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  // console.log(SAVING TASKS=>',tasks);
 }
 
-//on page load need to pull from storage
+//on page load, pull from storage or create a blank array
 function loadTasks() {
   var tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  console.log('LOADING TASKS=>', tasks)
   // go through time collection, for every element in time collection find every element to be populated with local stoage data 
   // loop through collection
   for (var i = 0; i < tasks.length; i++) {
@@ -138,8 +85,6 @@ function loadTasks() {
     // add the task description to the textarea from the local storage data
     taskTimeSlotTextarea.val(task.taskDescription);
   }
-
-
 }
 
 loadTasks();
